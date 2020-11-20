5af4270f6ad772741105abc5bb581d7cbdf8dd86
Rebekah <heal.rebekah@gmail.com>
2020-11-21 06:28:54 +1000

    fixed bug

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,11 +1,9 @@
 from controllers.items_controller import items
 from controllers.auth_controller import auth
 from controllers.orders_controller import orders
-from controllers.order_shipping_controller import order_shipping
 
 registerable_controllers = [
     auth, 
     items,
-    orders,
-    order_shipping
-]
+    orders
+    ]
